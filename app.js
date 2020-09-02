40e6c7fca2e9b6a09cc9acb87ce03208da48d4ac
venkat-raghulan <46018224+venkat-raghulan@users.noreply.github.com>
2020-09-02 23:48:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const svgHelmet = document.getElementById("svg-helmet");
 const svgMouthPlate = document.getElementById("svg-mouthplate");
 const svgBackGround = document.getElementById("svg-background");
 
+const resetButton = document.getElementById("reset");
+
+colorBoxes = document.querySelectorAll(".color-box");
+
 facePlate.onchange = () => {
   svgFacePlate.style.fill = facePlate.value;
 };
@@ -21,5 +25,15 @@ mouthPlate.onchange = () => {
 };
 
 background.onchange = () => {
+  svgBackGround.style.fill = background.value;
+};
+
+resetButton.onclick = () => {
+  colorBoxes.forEach((element) => {
+    element.value = "#ffffff";
+  });
+  svgFacePlate.style.fill = facePlate.value;
+  svgHelmet.style.fill = helmet.value;
+  svgMouthPlate.style.fill = mouthPlate.value;
   svgBackGround.style.fill = background.value;
 };
